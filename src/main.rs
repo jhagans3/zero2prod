1e237d7b857ddacc3cf7eef124e892402e535675
James Hagans <jameshagans@gravysolutions.io>
2020-11-15 14:29:09 -0500

    3.5.1.2 Choosing a randome port

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use zero2prod::run;
+use std::net::TcpListener;
+
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let address = "127.0.0.1:0";
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await
 }
