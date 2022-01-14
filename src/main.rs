54666c605052dd34bb0644c72b5b399a5d085a28
James Hagans <jhagans@acm.org>
2022-01-14 03:25:11 -0500

    small refactor
    Signed-off-by: James Hagans <jhagans@acm.org>

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@ use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
+    format!("Hello {name}!")
 }
 
-#[actix_web::main]
+#[tokio::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
